6fd37a2397d59418e6d8d2f9aec3c4309486e000
rajan235 <bansalrajan553@gmail.com>
2025-06-25 23:21:36 +0530

    Remove old files and keep only python_scripts structure

--- config/config.example.py
+++
@@ -1,8 +0,0 @@
-API_KEY = "Your_API_Key_Here"
-DB_CONFIG = {
-    "dbname": "your_database_name",
-    "user": "postgres",
-    "password": "your_password",
-    "host": "localhost",
-    "port": 5432
-}
--- src/strategies/growth_and_roce.py
+++
@@ -1,9 +0,0 @@
-def generate_signal(sales_growth, roce):
-    if sales_growth is None or roce is None:
-        return "No Data"
-    if sales_growth > 10 and roce > 10:
-        return "Buy"
-    elif sales_growth < 0 or roce < 5:
-        return "Sell"
-    else:
-        return "Hold"
